66ad95d214e019dc62d8ffa4dd818f1de7a4f0e7
sk521 <samkwon521@gmail.com>
2018-06-28 16:01:03 -0700

    divisible sum pair solved

---
+++ algo/implementation/divisibleSumPairs.js
@@ -0,0 +1,13 @@
+function divisibleSumPairs(n, k, ar) {
+  let foundPairs = 0;
+
+  for (let i = 0; i < ar.length; i++) {
+    for (let j = i + 1; j < ar.length; j++) {
+      if ((ar[i] + ar[j]) % k === 0) {
+        foundPairs++;
+      }
+    }
+  }
+
+  return foundPairs;
+}
